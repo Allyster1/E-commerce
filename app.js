0106dab71a72332e7ffd74cb674d053951c62871
Allyster <ovardovslav@gmail.com>
2024-03-23 19:22:07 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Dependencies
+const express = require("express");
+const app = express();
+
+// Modules
+const path = require("path");
+
+// Routes
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
+// Parse Incoming Data
+app.use(express.urlencoded({ extended: true }));
+
+// Forward access to the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Use Routes
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+// Display 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+// Adjust localhost if necessary
+app.listen(5500);
---
+++ utility/path.js
@@ -0,0 +1,4 @@
+const path = require("path");
+
+// Fix this
+module.exports = path.dirname(require.main.filename);
